6485b57a58bd3906b1eac6ed8399d811af2f2268
angelmicelti <angelmicelti@users.noreply.github.com>
2022-02-01 20:36:26 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+basic.forever(function () {
+    basic.showLeds(`
+        . # # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(100)
+})
